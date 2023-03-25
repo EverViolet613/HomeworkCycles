5cd8d22820baac4ff32e06e6b591bdf161e397ca
Yuliya Prokhorova <lostghost9751@gmail.com>
2023-03-25 19:26:50 +0300

    add parameterized test

--- src/test/java/ru/netology/services/FreelancerTest.java
+++ src/test/java/ru/netology/services/FreelancerTest.java
@@ -1,28 +1,21 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 
 public class FreelancerTest {
 
-    @Test
-    public void testTheCounter1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/freelance.csv")
+
+    public void testTheCounter(int expected, int income, int expenses, int threshold) {
         Freelancer service = new Freelancer();
 
-        int actual = service.calculate(10_000, 3_000, 20_000);
-        int expected = 3;
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void testTheCounter2() {
-        Freelancer service = new Freelancer();
-
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        int expected = 2;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
+
 }
